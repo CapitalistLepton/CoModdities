acb626400205c3a5d84366ed4255bc5be58f3192
CapitalistLepton <CapitalistLepton@users.noreply.github.com>
2018-01-03 21:58:01 -0800

    Add amounts to the market.

--- src/main/java/com/capitalistlepton/commodities/model/Market.java
+++ src/main/java/com/capitalistlepton/commodities/model/Market.java
@@ -5,6 +5,47 @@ import java.util.TreeMap;
 
 public class Market {
 	
+	/** Map of all possible resources on the market and their respective symbols. */
+	private SortedMap<String, Resource> resources;
+	/** Maps the symbol of all possible resources to the amount available in the market. */
+	private SortedMap<String, Integer> amounts;
+	
+	public Market() {
+		resources = new TreeMap<String, Resource>();
+		resources.put("LUM", new Resource("Lumber", "LUM", 1.0));
+		resources.put("IRO", new Resource("Iron", "IRO", 10.0));
+		resources.put("STO", new Resource("Stone", "STO", 1.0));
+		amounts = new TreeMap<String, Integer>();
+		for (String s: resources.keySet()) {
+			amounts.put(s, 50);
+		}
+	}
+	
+	/** Returns a String with all the symbols of resources and the prices separated by two spaces */
+	public String ticker() {
+		StringBuilder output = new StringBuilder();
+		for (Resource r: resources.values()) {
+			output.append(r);
+			output.append(" ");
+			output.append(amounts.get(r.getSym()));
+			output.append("u  ");
+		}
+		return output.toString();
+	}
+
+	/** 
+	 * Returns if the given symbol is traded on this market
+	 * 
+	 * @param symbol String symbol to check for
+	 * @return boolean value of whether the symbol is traded on this market
+	 */
+	public boolean contains(String symbol) {
+		if (symbol == null) {
+			return false;
+		}
+		return resources.containsKey(symbol);
+	}
+	
 	/** Interface for all actions that companies can perform on the market */
 	public interface Action {
 		public boolean action(Market m, Company co, String sym, int amount);
@@ -13,10 +54,11 @@ public class Market {
 	/** Action to buy a resource */
 	public static final Action BUY = new Action() {
 		public boolean action(Market m, Company co, String sym, int amount) {
-			if (co.getBalance() >= amount * m.resources.get(sym).getPrice()) {
+			if (amount <= m.amounts.get(sym) && co.getBalance() >= amount * m.resources.get(sym).getPrice()) {
 				try {
 					co.addResource(m.resources.get(sym), amount);
 					co.changeBalance(-1 * amount * m.resources.get(sym).getPrice());
+					m.amounts.put(sym, m.amounts.get(sym) - amount);
 					return true;
 				} catch (IllegalArgumentException e) {
 					System.out.println(e.getMessage());
@@ -35,6 +77,7 @@ public class Market {
 				try {
 					co.removeResource(m.resources.get(sym), amount);
 					co.changeBalance(amount * m.resources.get(sym).getPrice());
+					m.amounts.put(sym, m.amounts.get(sym) + amount);
 					return true;
 				} catch (IllegalArgumentException e) {
 					System.out.println(e.getMessage());
@@ -45,37 +88,4 @@ public class Market {
 			}
 		}
 	};
-	
-	/** Map of all possible resources on the market and their respective symbols */
-	private SortedMap<String, Resource> resources;
-	
-	public Market() {
-		resources = new TreeMap<String, Resource>();
-		resources.put("LUM", new Resource("Lumber", "LUM", 1.0));
-		resources.put("IRO", new Resource("Iron", "IRO", 10.0));
-		resources.put("STO", new Resource("Stone", "STO", 1.0));
-	}
-	
-	/** Returns a String with all the symbols of resources and the prices separated by two spaces */
-	public String ticker() {
-		StringBuilder output = new StringBuilder();
-		for (Resource r: resources.values()) {
-			output.append(r);
-			output.append("  ");
-		}
-		return output.toString();
-	}
-
-	/** 
-	 * Returns if the given symbol is traded on this market
-	 * 
-	 * @param symbol String symbol to check for
-	 * @return boolean value of whether the symbol is traded on this market
-	 */
-	public boolean contains(String symbol) {
-		if (symbol == null) {
-			return false;
-		}
-		return resources.containsKey(symbol);
-	}
 }
