439df2c3889ec7b904675d2cb4cbe3805d593cae
CapitalistLepton <CapitalistLepton@users.noreply.github.com>
2018-01-14 23:32:47 -0800

    Add database initialization files.

---
+++ src/db/create-tables.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS Companies;
+CREATE TABLE Companies (
+  cid INT PRIMARY KEY,
+  name VARCHAR(255),
+  balance FLOAT
+);
+
+DROP TABLE IF EXISTS Factories;
+CREATE TABLE Factories (
+  fid INT PRIMARY KEY,
+  name VARCHAR(255),
+  rid INT REFERENCES Recipes(rid)
+);
+
+DROP TABLE IF EXISTS FactoryList;
+CREATE TABLE FactoryList (
+  cid INT REFERENCES Companies(cid),
+  fid INT REFERENCES Factories(fid)
+);
+
+DROP TABLE IF EXISTS Recipes;
+CREATE TABLE Recipes (
+  rid INT PRIMARY KEY,
+  output INT REFERENCES Resources(eid),
+  output_amount INT
+);
+
+DROP TABLE IF EXISTS Requirements;
+CREATE TABLE Requirements (
+  rid INT REFERENCES Recipes(rid),
+  eid INT REFERENCES Resources(eid),
+  amount INT
+);
+
+DROP TABLE IF EXISTS Resources;
+CREATE TABLE Resources (
+  eid INT PRIMARY KEY,
+  name VARCHAR(255),
+  symbol VARCHAR(5),
+  price FLOAT
+);
+
+PRAGMA foreign_keys=ON;
+
+.mode csv
+.import resources.csv Resources
+.import recipes.csv Recipes
+.import factories.csv Factories
+.import requirements.csv Requirements
